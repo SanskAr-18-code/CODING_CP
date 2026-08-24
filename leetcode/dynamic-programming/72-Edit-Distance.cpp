1ebd0b2f51b7e0c285c48a52345cacdac6d60997
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-08-25 00:18:25 +0530

    Add Edit-Distance (dynamic-programming)

--- leetcode/dynamic-programming/72-Edit-Distance.cpp
+++ leetcode/dynamic-programming/72-Edit-Distance.cpp
@@ -1,20 +1,20 @@
 class Solution {
 private:
-    int ways(int i, int j, string& s1, string& s2,
-    vector<vector<int>> &dp) {
+    int ways(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
         if (i < 0)
             return j + 1;
         if (j < 0)
             return i + 1;
 
-        if(dp[i][j]!=-1) return dp[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
         if (s1[i] == s2[j]) {
-            return ways(i - 1, j - 1, s1, s2,dp);
+            return dp[i][j] = ways(i - 1, j - 1, s1, s2, dp);
         }
-        int insert = 1 + ways(i, j - 1, s1, s2,dp);
-        int replace = 1 + ways(i - 1, j - 1, s1, s2,dp);
-        int del = 1 + ways(i - 1, j, s1, s2,dp);
+        int insert = 1 + ways(i, j - 1, s1, s2, dp);
+        int replace = 1 + ways(i - 1, j - 1, s1, s2, dp);
+        int del = 1 + ways(i - 1, j, s1, s2, dp);
         return dp[i][j] = min(insert, min(replace, del));
     }
 
@@ -22,7 +22,26 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        return ways(n - 1, m - 1, word1, word2,dp);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        // return ways(n - 1, m - 1, word1, word2,dp);
+        // Tabulation
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[0][j] = j;
+                dp[i][0] = i;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int insert = 1 + dp[i][j-1];                    int replace = 1 + dp[i-1][j-1];
+                    int del = 1 + dp[i-1][j];
+                    dp[i][j]=min(insert,min(replace,del));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
