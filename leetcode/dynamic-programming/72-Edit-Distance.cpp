de41a3a1269f8eb76de23ae3844af58b585b7137
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-08-25 00:12:41 +0530

    Add Edit-Distance (dynamic-programming)

---
+++ leetcode/dynamic-programming/72-Edit-Distance.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    int ways(int i, int j, string& s1, string& s2,
+    vector<vector<int>> &dp) {
+        if (i < 0)
+            return j + 1;
+        if (j < 0)
+            return i + 1;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if (s1[i] == s2[j]) {
+            return ways(i - 1, j - 1, s1, s2,dp);
+        }
+        int insert = 1 + ways(i, j - 1, s1, s2,dp);
+        int replace = 1 + ways(i - 1, j - 1, s1, s2,dp);
+        int del = 1 + ways(i - 1, j, s1, s2,dp);
+        return dp[i][j] = min(insert, min(replace, del));
+    }
+
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n,vector<int> (m,-1));
+        return ways(n - 1, m - 1, word1, word2,dp);
+    }
+};
