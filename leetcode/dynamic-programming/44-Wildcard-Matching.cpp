e0c130b4781fee093125e21186e46743933c16f5
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-08-26 22:38:20 +0530

    Add Wildcard-Matching (dynamic-programming)

--- leetcode/dynamic-programming/44-Wildcard-Matching.cpp
+++ leetcode/dynamic-programming/44-Wildcard-Matching.cpp
@@ -24,7 +24,28 @@ public:
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        return match(n-1,m-1,s,p,dp);
+        vector<vector<bool>> dp(n+1,vector<bool> (m+1,0));
+        // return match(n-1,m-1,s,p,dp);
+        // Tabulation
+        // base case
+        dp[0][0]=true;
+        for(int j=1; j<=m; j++){
+            bool flag=true;
+            for(int k=1; k<=j ; k++){
+                if(p[k-1]!='*') flag=false; 
+            }
+            if(flag) dp[0][j]=true;
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if (s[i-1]==p[j-1] || p[j-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                    dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
