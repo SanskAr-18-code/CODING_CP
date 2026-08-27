fe7d827537341ed8b586305f27da091fe1e56894
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-08-27 12:38:00 +0530

    Add Best-Time-to-Buy-and-Sell-Stock (dynamic-programming)

--- leetcode/dynamic-programming/121-Best-Time-to-Buy-and-Sell-Stock.cpp
+++ leetcode/dynamic-programming/121-Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -1,19 +1,14 @@
 class Solution {
-private:
-    int profit(int idx,bool canbuy,vector<int> &prices,vector<vector<int>> &dp){
-        if(idx==prices.size()) return 0;
-        if(dp[idx][canbuy]!=-1) return dp[idx][canbuy];
-        if(canbuy){
-            return dp[idx][canbuy]=max(-prices[idx]+profit(idx+1,false,prices,dp),profit(idx+1,true,prices,dp));
-        }
-        else{
-            return dp[idx][canbuy]=max(prices[idx],profit(idx+1,false,prices,dp));
-        }
-    }
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>> dp(n,vector<int> (2,-1));
-        return profit(0,true,prices,dp);
+        int mn = prices[0];
+        int ans = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            ans = max(ans, prices[i] - mn);
+            mn = min(mn, prices[i]);
+        }
+
+        return ans;
     }
 };
