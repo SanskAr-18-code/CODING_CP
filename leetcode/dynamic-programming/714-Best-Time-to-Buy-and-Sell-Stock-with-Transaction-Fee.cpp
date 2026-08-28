ed5e798cf06ea382d7bae92af0f1f8832c491f3e
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-08-28 17:51:22 +0530

    Add Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee (dynamic-programming)

--- leetcode/dynamic-programming/714-Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.cpp
+++ leetcode/dynamic-programming/714-Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.cpp
@@ -8,7 +8,7 @@ public:
     {
         if (idx == arr.size())
             return 0;
-    if(dp[idx][canbuy]!=-1) return dp[idx][canbuy];
+
         if (canbuy)
         {
             int pick = -arr[idx] + profit(idx + 1, false, arr, dp, fee);
@@ -17,7 +17,7 @@ public:
         }
         else
         {
-            int pick = arr[idx] - fee + profit(idx + 1, true, arr, dp, fee);
+            int pick = arr[idx] - fee + profit(idx + 2, true, arr, dp, fee);
             int notpick = profit(idx + 1, false, arr, dp, fee);
             return dp[idx][canbuy] = max(pick, notpick);
         }
@@ -25,7 +25,23 @@ public:
     int maxProfit(vector<int> &arr, int fee)
     {
         int n = arr.size();
-        vector<vector<int>> dp(n,vector<int> (2,-1));
-        return profit(0,true,arr,dp,fee);
+        vector<vector<int>> dp(n+1,vector<int> (2,0));
+        // return profit(0,true,arr,dp,fee);
+        //Tabulation
+        for(int i=n-1; i>=0 ; i--){
+            for(int k=0; k<=1; k++){
+                if(k){
+                    int pick=-arr[i]+dp[i+1][false];
+                    int notpick=dp[i+1][true];
+                    dp[i][k]=max(pick,notpick);
+                }
+                else{
+                    int pick=arr[i]-fee+dp[i+1][true];
+                    int notpick=dp[i+1][false];
+                    dp[i][k]=max(pick,notpick);
+                }
+            }
+        }
+        return dp[0][true];
     }
 };
