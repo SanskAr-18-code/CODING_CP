03f081abbc628747fc27f4d57ad3d1ec2ba2dfbf
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-08-28 17:02:59 +0530

    Add Best-Time-to-Buy-and-Sell-Stock-III (dynamic-programming)

--- leetcode/dynamic-programming/123-Best-Time-to-Buy-and-Sell-Stock-III.cpp
+++ leetcode/dynamic-programming/123-Best-Time-to-Buy-and-Sell-Stock-III.cpp
@@ -1,36 +1,27 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution
-{
-private:
-    int profit(int idx, bool canbuy, int trans, vector<int> &prices,
-    vector<vector<vector<int>>> &dp)
-    {
-        if (idx == prices.size())
-            return 0;
-        if (trans == 2)
-            return 0;
-        if(dp[idx][canbuy][trans]!=-1) return dp[idx][canbuy][trans];
-        if (canbuy)
-        {
-            int pick = -prices[idx] + profit(idx + 1, false, trans, prices,dp);
-            int notpick = profit(idx + 1, true, trans, prices,dp);
-            return dp[idx][canbuy][trans]=max(pick, notpick);
-        }
-        else
-        {
-            int pick = prices[idx] + profit(idx + 1, true, trans+1, prices,dp);
-            int notpick = profit(idx + 1, false, trans, prices,dp);
-            return dp[idx][canbuy][trans]=max(pick, notpick);
-        }
-    }
-
+class Solution {
 public:
     int maxProfit(vector<int> &prices)
     {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int> (3,-1)));
-        return profit(0,true,0,prices,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int> (3,0)));
+        // return profit(0,true,0,prices);
+        //Tabulation
+        for (int idx=n-1; idx>=0 ; idx--){
+            for(int buy=0; buy<=1; buy++){
+                for(int trans=1; trans<=2; trans++){
+                    if(buy==1){
+                        int pick=-prices[idx]+dp[idx+1][false][trans];
+                        int notpick=dp[idx+1][true][trans];
+                        dp[idx][buy][trans]=max(pick,notpick);
+                    }
+                    else{
+                        int pick=prices[idx]+dp[idx+1][true][trans-1];
+                        int notpick=dp[idx+1][false][trans];
+                        dp[idx][buy][trans]=max(pick,notpick);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
