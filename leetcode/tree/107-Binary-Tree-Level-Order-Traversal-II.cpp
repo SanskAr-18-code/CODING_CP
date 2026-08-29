eb28b8299c50ef7b5ce092f6883c28c3b541d612
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-08-29 11:48:09 +0530

    Add Binary-Tree-Level-Order-Traversal-II (tree)

--- leetcode/tree/107-Binary-Tree-Level-Order-Traversal-II.cpp
+++ leetcode/tree/107-Binary-Tree-Level-Order-Traversal-II.cpp
@@ -6,34 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        stack<vector<int>> st;
         queue<TreeNode*> q;
         vector<vector<int>> res;
-        if(!root) return {};
+        if (!root)
+            return {};
         q.push(root);
-        while(!q.empty()){
-            int n=q.size();
+        while (!q.empty()) {
+            int n = q.size();
             vector<int> vec;
-            for(int i=1; i<=n; i++){
-                auto it=q.front();
+            for (int i = 1; i <= n; i++) {
+                auto it = q.front();
                 q.pop();
                 vec.push_back(it->val);
-                if(it->left) q.push(it->left);
-                if (it->right) q.push(it->right);
+                if (it->left)
+                    q.push(it->left);
+                if (it->right)
+                    q.push(it->right);
             }
-            st.push(vec);
+            res.push_back(vec);
         }
-        while(!st.empty()){
-            auto it=st.top();
-            st.pop();
-            res.push_back(it);
-        }
+        reverse(res.begin(),res.end());
         return res;
     }
 };
