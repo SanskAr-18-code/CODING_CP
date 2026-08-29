f0c0aadc813a36367a8571db23b5587bd8dfee9c
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-08-30 01:06:46 +0530

    Add Path-Sum-III (tree)

--- leetcode/tree/437-Path-Sum-III.cpp
+++ leetcode/tree/437-Path-Sum-III.cpp
@@ -1,45 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool isleaf(TreeNode* root){
-        return (root->left==nullptr && root->right==nullptr);
+    long long cnt = 0;
+    long long prefix = 0;
+
+    unordered_map<long long,long long> mp;
+    void dfs(TreeNode* root, long long target)
+    {
+        if(root == nullptr)
+            return;
+        prefix += root->val;
+        cnt += mp[prefix - target];
+        mp[prefix]++;
+
+        dfs(root->left, target);
+        dfs(root->right, target);
+        mp[prefix]--;
+        prefix -= root->val;
     }
 
-    void paths(TreeNode* root,long long target,int &total){
-        if(!root) return;
-        target-=root->val;
-        if(target==0) total+=1;
-        if(isleaf(root)) return;
-        paths(root->left,target,total);
-        paths(root->right,target,total);
-    }
     int pathSum(TreeNode* root, int targetSum) {
-        int total=0;
-        queue<TreeNode*> q;
-        if(!root) return 0;
-        q.push(root);
-        while(!q.empty()){
-            int n=q.size();
-            for(int i=1 ; i<=n; i++){
-                auto it=q.front();
-                q.pop();
-                int sum=0;
-                paths(it,targetSum,sum);
-                total+=sum;
-                if(it->left) q.push(it->left);
-                if(it->right) q.push(it->right);
-            }
-        }
-        return total;
+        mp[0] = 1;
+        dfs(root, targetSum);
+
+        return cnt;
     }
 };
