dd19c3e2928c44bf1e88ae2a43e9038057616527
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-08-30 11:58:54 +0530

    Add Longest-Increasing-Subsequence (binary-search)

--- leetcode/binary-search/300-Longest-Increasing-Subsequence.cpp
+++ leetcode/binary-search/300-Longest-Increasing-Subsequence.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    int len(int idx,vector<int> &nums,int pre,vector<vector<int>> &dp){
-        if(idx==nums.size()) return 0;
-        if(dp[idx][pre]!=-1) return dp[idx][pre];
-        if(idx==pre  ||  nums[idx]<=nums[pre]){
-            return dp[idx][pre]=len(idx+1,nums,pre,dp);
-        }
-        else{
-            int nottake=len(idx+1,nums,pre,dp);
-            int take = 1+len(idx+1,nums,idx,dp);
-            return dp[idx][pre]=max(take,nottake);
+    int len(int idx, vector<int>& nums, int pre, vector<vector<int>>& dp) {
+        if (idx == nums.size())
+            return 0;
+        if (dp[idx][pre] != -1)
+            return dp[idx][pre];
+        if (idx == pre || nums[idx] <= nums[pre]) {
+            return dp[idx][pre] = len(idx + 1, nums, pre, dp);
+        } else {
+            int nottake = len(idx + 1, nums, pre, dp);
+            int take = 1 + len(idx + 1, nums, idx, dp);
+            return dp[idx][pre] = max(take, nottake);
         }
     }
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        int maxlen=1;
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        for(int i=0; i<n ; i++){
-            maxlen=max(maxlen,1+len(i,nums,i,dp));
+        int n = nums.size();
+        int maxlen = 1;
+        vector<vector<int>> dp(n + 1, vector<int>(n, 0));
+        // for(int i=0; i<n ; i++){
+        //     maxlen=max(maxlen,1+len(i,nums,i,dp));
+        // }
+        // Tabulation
+        for (int i = n - 1; i >= 0; i--) {
+            for (int k = i; k >= 0; k--) {
+                if (i == k || nums[i] <= nums[k]) {
+                    dp[i][k] = dp[i + 1][k];
+                } else {
+                    int nottake = dp[i + 1][k];
+                    int take = 1 + dp[i + 1][i];
+                    dp[i][k] = max(nottake, take);
+                }
+            }
         }
-        //tabulation
-        return maxlen;
+        int ans = 1;
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, 1 + dp[i][i]);
+        }
+        return ans;
     }
 };
