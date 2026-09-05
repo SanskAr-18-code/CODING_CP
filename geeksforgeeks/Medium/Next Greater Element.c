89410175fbcbea717f80079cce6f332dbc0ab3e3
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-09-05 11:24:01 +0530

    Add Next Greater Element (Medium)

---
+++ geeksforgeeks/Medium/Next Greater Element.c
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        stack<int> st;
+        int n=arr.size();
+        vector<int> nums(n,-1);
+        for(int i=n-1; i>=0; i--){
+            if(st.empty()){
+                st.push(arr[i]);
+                continue;
+            }
+            while(!st.empty() && st.top()<=arr[i]){
+                st.pop();
+            }
+            if(!st.empty()) nums[i]=st.top();
+            st.push(arr[i]);
+        }
+        return nums;
+    }
+};
