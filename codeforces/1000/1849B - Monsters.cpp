58e177de054b81c3ef13a1411250f11994d87841
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-09-06 15:29:22 +0530

    Add Monsters (1000)

---
+++ codeforces/1000/1849B - Monsters.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<pair<int,int>> vec;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        int rem=a%k;
+        if(rem==0){
+            rem=k;
+        }
+        vec.push_back({-rem,i+1});
+    }
+    sort(vec.begin(),vec.end());
+    for(auto it: vec){
+        cout<<it.second<<" ";
+    }
+    cout<<endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
