48d1ff2335b1ab573ea61c160b2dc3273d50250b
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-09-07 16:48:46 +0530

    Add Stack using Queue (Medium)

---
+++ geeksforgeeks/Medium/Stack using Queue.cpp
@@ -0,0 +1,30 @@
+class myStack {
+	queue<int> q;
+	
+	public:
+	void push(int x) {
+		q.push(x);
+		
+		int sz = q.size();
+		
+		for (int i = 0; i < sz - 1; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+	}
+	void pop() {
+		if (!q.empty())
+			q.pop();
+	}
+	
+	int top() {
+		if (q.empty()) {
+			return - 1;
+		}
+		return q.front();
+	}
+	
+	int size() {
+		return q.size();
+	}
+};
