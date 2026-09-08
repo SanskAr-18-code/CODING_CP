fc4db7fc5020ce9a0591efac763d98442e7a8baf
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-09-08 21:40:53 +0530

    Add Binary-Tree-Postorder-Traversal (tree)

--- leetcode/tree/145-Binary-Tree-Postorder-Traversal.cpp
+++ leetcode/tree/145-Binary-Tree-Postorder-Traversal.cpp
@@ -6,20 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-vector<int> res;
-    void postorder(TreeNode* root){
-        if(!root) return;
-        postorder(root->left);
-        postorder(root->right);
-        res.push_back(root->val);
-    }
     vector<int> postorderTraversal(TreeNode* root) {
-        postorder(root);
+        stack<TreeNode*> st;
+        vector<int> res;
+        if (!root)
+            return {};
+        st.push(root);
+        unordered_map<TreeNode*, bool> vis;
+        vis[root] = true;
+        while (!st.empty()) {
+            auto it = st.top();
+            if ((!it->left || vis[it->left]) && (!it->right || vis[it->right])) {
+                res.push_back(it->val);
+                st.pop();
+            }
+            if (it->right && !vis[it->right]) {
+                st.push(it->right);
+                vis[it->right] = true;
+            }
+            if (it->left && !vis[it->left]) {
+                st.push(it->left);
+                vis[it->left] = true;
+            }
+        }
         return res;
     }
 };
