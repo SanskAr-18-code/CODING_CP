b1e3fb73308d0103c1aa4ef196a6a9f5088c596a
SanskAr-18-code <sanskarboralkar18@gmail.com>
2026-09-26 12:39:47 +0530

    Add Basketball Together (1000)

---
+++ codeforces/1000/1725B - Basketball Together.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int n, d;
+    cin >> n >> d;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+
+    int i = 0;
+    int j = n - 1;
+    int ans = 0;
+
+    while (i <= j)
+    {
+        int need = d / arr[j];
+
+        if (i + need <= j)
+        {
+            ans++;
+            i += need;
+            j--;
+        }
+        else
+        {
+            break;
+        }
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
